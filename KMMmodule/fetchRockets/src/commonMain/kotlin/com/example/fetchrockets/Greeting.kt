74f6310a682e05b80a3bf2af520c79a584f26941
Martin Ficek <martin.ficek2002@gmail.com>
2023-07-04 00:42:37 +0200

    Deleted Unnecesarry folder

--- KMMmodule/fetchRockets/src/commonMain/kotlin/com/example/fetchrockets/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fetchrockets
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello AAA, ${platform.name}!"
-    }
-}
